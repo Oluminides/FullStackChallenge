ce4a9d7752ed9126887e96cbe4323eca5e83a2a8
Oluminides <olumideolabimtan@gmail.com>
2020-05-09 20:09:55 -0600

    Generated authentication scaffolding

--- FullStackChallenge/resources/views/welcome.blade.php
+++ FullStackChallenge/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Full Stack Dev Challenge
                 </div>
 
                 <div class="links">
